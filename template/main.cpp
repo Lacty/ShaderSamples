ce13c188b41a9e4841373fee28de862335ba0d4b
Lacty <akira206@gmail.com>
2017-02-21 18:06:11 +0900

    update comment

--- template/main.cpp
+++ template/main.cpp
@@ -6,10 +6,28 @@ int main() {
   App app(360, 360);
   app.setBgColor({0.4, 0.4, 0.4, 1.0});
   
+  // vtx array(rect
+  static const GLfloat vtx[] = {
+    -0.5, -0.5,
+    +0.5, -0.5,
+    +0.5, +0.5,
+    -0.5, +0.5
+  };
+               
+  auto drawRect = [] (const GLfloat* vtx)->void {
+    glVertexPointer(2, GL_FLOAT, 0, vtx);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glDrawArrays(GL_QUADS, 0, 4);
+    glDisableClientState(GL_VERTEX_ARRAY);
+  };
+  
   Shader shader("v_shader.vsp", "f_shader.fsp");
+  shader.on();
   
   while(app.isOpen()) {
     app.begin();
+    
+    drawRect(vtx);
     
     app.end();
   }
