0c1f582f2893c52fce0be849ebb209008dec0c07
Lacty <akira206@gmail.com>
2017-02-22 14:39:16 +0900

    rename .vsp to vert

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -6,7 +6,7 @@ int main() {
   App app(360, 360);
   app.setBgColor({0.4, 0.4, 0.4, 1.0});
   
-  Shader shader("data/v_shader.vsp", "data/f_shader.fsp");
+  Shader shader("data/v_shader.vert", "data/f_shader.frag");
   
   while(app.isOpen()) {
     app.begin();
@@ -14,10 +14,7 @@ int main() {
     shader.on();
     drawRect(0, 0, 2);
     shader.off();
-    
-    glColor3f(1, 1, 1);
-    drawPoint(0, 0, 5);
-    
+        
     app.end();
   }
 }
